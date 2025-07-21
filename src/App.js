68c962d66bd1fba557622e2ce1c55b78fc092614
MohamedIhabIbrahim22 <mohamedelrawy007@gmail.com>
2025-07-21 07:06:19 +0300

    Routes update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ class App extends Component {
         <Routes>
           <Route exact path='/' element={<Navigate to="/login"/>}/>
           <Route exact path='/login' element={<Login/>}/>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/activate' element={<Activate />} />
           <Route exact path='/resetpassword' element={<ResetPassword/>}/>
           <Route exact path='/passwordchange' element={<PasswordChange/>}/>
         </Routes>
